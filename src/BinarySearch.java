19a2ed3d188c0bf1a668185819efd7a86d2ee150
Muthuramakrishnan M <muthu.rama.krishnan96@gmail.com>
2022-10-10 23:03:10 +0530

    BinarySearch fix

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -1,14 +1,17 @@
+/* package codechef; // don't place package name! */
+
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class BinarySearch {
-    public static int getIndexOf(int[] arr, int k) {
+/* Name of the class has to be "Main" only if the class is public. */
+public class BinarySearch
+{
+    public static int getIndexOf(int[] arr, int k){
         int low = 0, high = arr.length - 1;
         while (low <= high) {
             int mid = (low + high) / 2;
             if (arr[mid] == k) {
-                return mid + 1;
+                return mid;
             }
             if (arr[mid] > k) {
                 high = mid - 1;
@@ -18,16 +21,23 @@ public class BinarySearch {
         }
         return -1;
     }
-
-    public static void main(String[] args) throws IOException {
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        // your code goes here
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine().strip());
+
+        int[] ipArray = new int[n];
         String[] s = br.readLine().split(" ");
-        int n = s.length;
-        int[] arr = new int[n];
-        int k = Integer.parseInt(br.readLine());
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(s[i]);
+        for(int i=0; i<n; i++){
+            ipArray[i] = Integer.parseInt(s[i]);
         }
-        System.out.println(getIndexOf(arr, k));
+
+        int T = Integer.parseInt(br.readLine().strip());
+        while(T>=1){
+            int k = Integer.parseInt(br.readLine().strip());
+            System.out.println(getIndexOf(ipArray, k));
+            T--;
+        }
     }
 }
